6619d1b3c645be48f3aa037505e2ad5bb419160a
JaehyeonCho93 <hooyaa93@gmail.com>
2023-02-19 17:36:24 +0900

    ListView : method 분리 및 스타일링

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,9 +4,7 @@ import 'package:toonflix/services/api_service.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
-//error : This class (or a class that this class inherits from) is marked as '@immutable',
-// but one or more of its instance fields aren't final: HomeScreen.webtoons
-// StatelessWidget인데 아래 webtoons를 상수 선언 안해줘서 그럼 final 추가
+
   final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
@@ -26,45 +24,70 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            //## ListView : 많은 아이템을 보여줄 땐 column이나 row대신 ListView가 적당
-            // 아래는 ListView 기초적 사용예제
-            // return ListView(
-            //   children: [
-            //     for (var webtoon in snapshot.data!) Text(webtoon.title)
-            //   ],
-            // ); //근데 한번에 데이터 다 불러와서 메모리 부하 가능
-
-            // ## ListView.builder는 viewport에 있는 데이터만 불러오고 벗어나면 삭제
-            // return ListView.builder(
-            //   scrollDirection: Axis.horizontal,
-            //   itemCount: snapshot.data!.length,
-            //   itemBuilder: (context, index) {
-            //     //index 덕분에 해당되는 데이터만 불러오기 가능해짐
-            //     print(index); // 스크롤하고 디버그 콘솔에서 확인 가능.
-            //     var webtoon = snapshot.data![index];
-            //     return Text(webtoon.title);
-            //   },
-            // );
-
-            return ListView.separated(
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  var webtoon = snapshot.data![index];
-                  return Text(webtoon.title);
-                },
-                // itemBuilder의 위젯 사이에 놓여질 위젯 생성
-                separatorBuilder: ((context, index) {
-                  return const SizedBox(
-                    width: 20,
-                  );
-                }),
-                itemCount: snapshot.data!.length);
+            //extract method를 통해 코드 분리
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                //Expanded를 이용해서 높이를 제한(다른 위젯이 사용하고 남은 공간을 채우도록)
+                Expanded(child: makeList(snapshot)),
+              ],
+            );
           }
           return const Center(
             child: CircularProgressIndicator(),
           );
         },
       ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 20,
+      ),
+      itemBuilder: (context, index) {
+        var webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    offset: const Offset(10, 10),
+                    color: Colors.black.withOpacity(0.5),
+                  )
+                ],
+              ),
+              child: Image.network(webtoon.thumb),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              webtoon.title,
+              style: const TextStyle(
+                fontSize: 22,
+              ),
+            ),
+          ],
+        );
+      },
+      // itemBuilder의 위젯 사이에 놓여질 위젯 생성
+      separatorBuilder: ((context, index) {
+        return const SizedBox(
+          width: 40,
+        );
+      }),
     );
   }
 }
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,28 +3,6 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:toonflix/models/webtoon_model.dart';
 
-//https://youtu.be/17Oh028Jpis?t=325 static에 대한 영상
-/*
-class Car {
-  static int wheels = 4;
-  Car (required this.wheels) // 접근 불가
-}
-var z4 = Car(4)
-z4.wheels // 에러 접근 불가.
-
-* static은 내부나 외부에서 this.를 통해서 접근이 안됨.
-직접 해당 클래스를 통해서 접근이 가능
-
-class Car {
-  static int wheels = 4;
-  Car (required Car.wheels) 
-}
-var z4 = Car(4)
-Car.wheels //4
-
-이번 코드에서 static을 사용한 이유는 매번 ApiService를 사용하기 위해서
-인스턴스를 만들필요 없이 ApiService.gettodaysToons() 이런식으로 사용할 수 있도록 하기 위함.
- */
 class ApiService {
   static const String baseUrl =
       "https://webtoon-crawler.nomadcoders.workers.dev";
