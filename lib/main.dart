20beeb357bca662901c4ffab2edb7b450648066e
JaehyeonCho93 <hooyaa93@gmail.com>
2023-02-19 15:24:58 +0900

    widget key 설정 & AppBar 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  //widget은 key를 가지고 있으며 ID처럼 사용된다.
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
