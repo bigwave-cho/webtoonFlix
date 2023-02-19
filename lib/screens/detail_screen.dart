134f8ef1e46d3f8e8236cde28482a5135522a350
JaehyeonCho93 <hooyaa93@gmail.com>
2023-02-19 21:36:13 +0900

    likeBtn : shared_preference

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:toonflix/models/webtoon_detail_model.dart';
 import 'package:toonflix/models/webtoon_episode.dart';
 import 'package:toonflix/services/api_service.dart';
 import 'package:toonflix/widgets/episode.dart';
 
 /*
-- url_launcher
-https://pub.dev/packages/url_launcher
-에피소드 탭 -> 해당 URL로 이동
-설치하고 문서에 따라 ios/ android configuration 해주기.
-
-리빌드!
-
-sms, tel mailto 등등 지원해줌
-
+좋아요 기능 추가하기
+shared_preferences
+로컬 기기에 정보 저장함.
  */
 class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
@@ -33,11 +28,49 @@ class _DetailScreenState extends State<DetailScreen> {
   late Future<WebtoonDetailModel> webtoon;
   late Future<List<WebtoonEpisodeModel>> episodes;
 
+  late SharedPreferences prefs;
+  bool isLiked = false;
+
+  //tip PlatformException (PlatformException(channel-error, Unable to establish connection on channel., null, null))
+  // 이런 에러 나오면 다운 받은 패키지가 어플에 적용이 안된거라 재시작
+
+  Future initPrefs() async {
+    prefs = await SharedPreferences.getInstance();
+    final likedToons = prefs.getStringList('likedToons');
+    if (likedToons != null) {
+      if (likedToons.contains(widget.id) == true) {
+        setState(() {
+          isLiked = true;
+        });
+      }
+    } else {
+      //비었으면 "likedToons"이름으로 string 어레이 생성
+      await prefs.setStringList('likedToons', []);
+    }
+  }
+
   @override
   void initState() {
     super.initState();
     webtoon = ApiService.getToonById(widget.id);
     episodes = ApiService.getLatestEpisodesById(widget.id);
+
+    initPrefs();
+  }
+
+  onHeartTap() async {
+    final likedToons = prefs.getStringList('likedToons');
+    if (likedToons != null) {
+      if (isLiked) {
+        likedToons.remove(widget.id);
+      } else {
+        likedToons.add(widget.id);
+      }
+      await prefs.setStringList('likedToons', likedToons);
+    }
+    setState(() {
+      isLiked = !isLiked;
+    });
   }
 
   @override
@@ -52,6 +85,13 @@ class _DetailScreenState extends State<DetailScreen> {
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
         elevation: 2,
+        actions: [
+          IconButton(
+            onPressed: onHeartTap,
+            icon:
+                Icon(isLiked ? Icons.favorite_rounded : Icons.favorite_outline),
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Padding(
