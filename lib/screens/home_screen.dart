0475ee485d679ddb355c27c75548ef6ce9c17b7b
JaehyeonCho93 <hooyaa93@gmail.com>
2023-02-19 16:52:41 +0900

    API: 기초적 방법(without FutureBuilder)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_model.dart';
+import 'package:toonflix/services/api_service.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+// 기초적인 데이터 받아오기 방법
+class _HomeScreenState extends State<HomeScreen> {
+  List<WebtoonModel> webtoons = [];
+  bool isLoading = true;
+
+  void waitForWebToons() async {
+    // ApiService통해서 http.get
+    webtoons = await ApiService.getTodaysToons();
+    isLoading = false;
+    //렌더링
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    waitForWebToons();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print(webtoons);
+    print(isLoading);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,26 +3,42 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:toonflix/models/webtoon_model.dart';
 
-class ApiService {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
-  final String today = "today";
+//https://youtu.be/17Oh028Jpis?t=325 static에 대한 영상
+/*
+class Car {
+  static int wheels = 4;
+  Car (required this.wheels) // 접근 불가
+}
+var z4 = Car(4)
+z4.wheels // 에러 접근 불가.
 
-//async함수는 Future타입을 반환
-  Future<List<WebtoonModel>> getTodaysToons() async {
-    // async : 비동기 함수로 await 지정 시 해당 await문은 동기적으로 실행됨.
+* static은 내부나 외부에서 this.를 통해서 접근이 안됨.
+직접 해당 클래스를 통해서 접근이 가능
+
+class Car {
+  static int wheels = 4;
+  Car (required Car.wheels) 
+}
+var z4 = Car(4)
+Car.wheels //4
+
+이번 코드에서 static을 사용한 이유는 매번 ApiService를 사용하기 위해서
+인스턴스를 만들필요 없이 ApiService.gettodaysToons() 이런식으로 사용할 수 있도록 하기 위함.
+ */
+class ApiService {
+  static const String baseUrl =
+      "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String today = "today";
+
+  static Future<List<WebtoonModel>> getTodaysToons() async {
     List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
-      //response의 body프로퍼티의 타입은 String임을 확인.
-      //따라서 json 으로 decode -> dynamic타입으로 변환(any)해서 반환
       final List<dynamic> webtoons = jsonDecode(response.body);
       for (var webtoon in webtoons) {
-        //webtoon -> {title:...}
-        //api로부터 json을 WebtoonModle의 named constructor fromJson을 통해서 인스턴스 생성.
         final toon = WebtoonModel.fromJson(webtoon);
-        //모델 클래스를 통해 초기화된 클래스를 toon에 할당하여 인스턴스 생성.
         webtoonInstances.add(toon);
       }
       return webtoonInstances;
