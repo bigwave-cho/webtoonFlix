513567c73026e3cd86327aad3cdd6afa65b29190
JaehyeonCho93 <hooyaa93@gmail.com>
2023-02-19 18:24:31 +0900

    API: webtoon detail, episodes

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/models/webtoon_episode.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
@@ -20,6 +22,35 @@ class ApiService {
         webtoonInstances.add(toon);
       }
       return webtoonInstances;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse('$baseUrl/$id');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+      // model은 응답받은 데이터를 decode해서 클래스화 시켜 코드 분리 해놓은거라 생각하면 됨.
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(
+      String id) async {
+    List<WebtoonEpisodeModel> episodesInstances = [];
+
+    final url = Uri.parse('$baseUrl/$id/episodes');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodesInstances.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      // model은 응답받은 데이터를 decode해서 클래스화 시켜 코드 분리 해놓은거라 생각하면 됨.
+
+      return episodesInstances;
     }
     throw Error();
   }
